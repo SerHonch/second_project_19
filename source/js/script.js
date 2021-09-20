1b2f618d94dbdf89cb0e6b8c2e7fe0da44ecbd06
Serhii Honcharuk <serhiihoncharuk@gmail.com>
2021-09-20 13:46:37 +0300

    Script fix

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,40 @@
 $(document).ready(function(){
     $('.header__mobile--btn').on('click', ()=>{
-        console.log('123');
+       $('.header__logo').toggleClass('header__logo--close')
     })
 
-
+   
 });
+let listItems = document.querySelectorAll('.specialization__item');
+
+listItems.forEach((it)=>{
+    it.addEventListener('click', function(){
+        it.classList.toggle('specialization__item--active');
+        it.querySelector('.specialization__text').classList.toggle('specialization__text--hidden');
+})
+})
+const swiper = new Swiper ('.swiper',{
+    direction: 'horizontal',
+    loop: true, 
+    slidesPerView: 4.5,
+    spaceBetween: 30,
+    grabCursor: true,
+
+})
+
+const mainSwiper = new Swiper ('.project__swiper',{
+    direction: 'horizontal',
+    slidesPerView: 'auto', 
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.btn__next',
+        prevEl: '.btn__prev',
+    },
+    grabCursor: true,
+    longSwipes: false,
+    // mousewheel: true,
+    // releaseOnEdges: true,
+    // draggable: true,
+    
+
+})
